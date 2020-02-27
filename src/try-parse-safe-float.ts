bbba5fb3012e56e60246b85ff61cb5f7d43041f0
Troy Gerwien <yortus@gmail.com>
2020-02-27 15:29:02 +0800

    better float parsing, all tests passing

--- src/try-parse-safe-float.ts
+++ src/try-parse-safe-float.ts
@@ -3,42 +3,48 @@ export function tryParseSafeFloat(rawNum: string): number | undefined {
     // Use built-in float parser to get the result.
     let result = Number.parseFloat(rawNum);
 
-    // Reject immediately if the result is NaN or |result| > 1.8e308
-    if (!Number.isFinite(result) || Math.abs(result) > 1.7e308) return;
+    // Reject immediately if the result is NaN or +/-Infinity
+    if (!Number.isFinite(result)) return;
 
     // Reject if the string is ill-formed, including if it contains invalid trailing characters.
     if (!FLOAT_REGEX.test(rawNum)) return;
 
-    // Reject if there are more than 15 significant decimal digits or if the exponent is less than -308.
-    // NB: This is a bit conservative, and will reject some numbers that can be represented exactly.
-    // However it never accepts numbers that cannot be represented exactly.
-    const len = rawNum.length;
-    let totalDigits = 0;
-    let leadingZeroes = 0;
-    let trailingZeroes = 0;
+    // Convert the parsed number back to a string. If the resulting string has an identical sequence of significant
+    // digits to the original string, then the number can be represented exactly, so return it. Otherwise reject the
+    // number, since it cannot be represented without loss of precision.
+    let newNum = String(result);
+    let [rawPos, rawLastIndex] = findFirstAndLastSignificantDigits(rawNum);
+    let [newPos, newLastIndex] = findFirstAndLastSignificantDigits(newNum);
+    if (rawPos === -1 && newPos === -1) return 0;
+    if (rawPos === -1 || newPos === -1) return;
+    while (true) {
+        let rawChar = rawNum.charCodeAt(rawPos);
+        if (rawChar === DECIMAL_POINT) rawChar = rawNum.charCodeAt(++rawPos);
+        let newChar = newNum.charCodeAt(newPos);
+        if (newChar === DECIMAL_POINT) newChar = newNum.charCodeAt(++newPos);
+        if (rawChar !== newChar) return;
+        if (rawPos === rawLastIndex && newPos === newLastIndex) return result;
+        if (rawPos === rawLastIndex || newPos === newLastIndex) return;
+        ++rawPos;
+        ++newPos;
+    }
+}
+
+
+function findFirstAndLastSignificantDigits(str: string): [number, number] {
+    let len = str.length;
+    let first = -1;
+    let last = -1;
     for (let pos = 0; pos < len; ++pos) {
-        let charCode = rawNum.charCodeAt(pos);
+        let charCode = str.charCodeAt(pos);
         if (charCode === LOWERCASE_E || charCode === UPPERCASE_E) break;
         if (charCode < ZERO_DIGIT || charCode > NINE_DIGIT) continue;
-        if (charCode === ZERO_DIGIT) {
-            ++trailingZeroes;
-            if (leadingZeroes === totalDigits) ++leadingZeroes;
+        if (charCode !== ZERO_DIGIT) {
+            if (first === -1) first = pos;
+            last = pos;
         }
-        else {
-            trailingZeroes = 0;
-        }
-        ++totalDigits;
     }
-
-    // NB: if the string is all zeros, then leading/trailing will overlap, but that doesn't matter for the check here.
-    let significantDigits = totalDigits - leadingZeroes - trailingZeroes;
-    if (significantDigits > 15) return;
-
-    // Reject if the result is zero but there are non-zero digits in the string. The number is too small to represent.
-    if (result === 0 && significantDigits > 0) return;
-
-    // The string representation passes all checks. Return the resulting number.
-    return result;
+    return [first, last];
 }
 
 
@@ -47,3 +53,4 @@ const LOWERCASE_E = 'e'.charCodeAt(0);
 const UPPERCASE_E = 'E'.charCodeAt(0);
 const ZERO_DIGIT = '0'.charCodeAt(0);
 const NINE_DIGIT = '9'.charCodeAt(0);
+const DECIMAL_POINT = '.'.charCodeAt(0);
--- src/try-parse.test.ts
+++ src/try-parse.test.ts
@@ -36,11 +36,11 @@ const tests = {
 
     // Well-formed strings
     '1000000000000000000000000000000000000000': {int: INVALID, float: '1e+39'},
-    [JSON.stringify(2**53 + 5)]: {int: INVALID, float: INVALID},
-    '9007199254740992': {int: INVALID, float: INVALID}, // too large for int, too many sig. digits for float
-    '9007199254740993': {int: INVALID, float: INVALID}, // too large for int, too many sig. digits for float
-    '-9007199254740992': {int: INVALID, float: INVALID}, // too small for int, too many sig. digits for float
-    '-9007199254740993': {int: INVALID, float: INVALID}, // too small for int, too many sig. digits for float
+    [JSON.stringify(2**53 + 5)]: {int: INVALID, float: '9007199254740996'},
+    '9007199254740992': {int: INVALID, float: '9007199254740992'}, // too large for int
+    '9007199254740993': {int: INVALID, float: INVALID}, // too large for int, loss of precision as float
+    '-9007199254740992': {int: INVALID, float: '-9007199254740992'}, // too small for int
+    '-9007199254740993': {int: INVALID, float: INVALID}, // too small for int, loss of precision as float
     '0': {int: '0', float: '0'},
     '-1': {int: '-1', float: '-1'},
     '+1': {int: '1', float: '1'},
@@ -55,32 +55,30 @@ const tests = {
     '-9999999999': {int: '-9999999999', float: '-9999999999'},
     '10000000000000': {int: '10000000000000', float: '10000000000000'},
     '-10000000000000': {int: '-10000000000000', float: '-10000000000000'},
-    [JSON.stringify(2**53 - 5)]: {int: '9007199254740987', float: INVALID}, // safe int, too many sig. digits for float
+    [JSON.stringify(2**53 - 5)]: {int: '9007199254740987', float: '9007199254740987'},
     [JSON.stringify(-(2 ** 40))]: {int: '-1099511627776', float: '-1099511627776'},
-    '9007199254740991': {int: '9007199254740991', float: INVALID}, // safe int, too many sig. digits for float
-    '-9007199254740991': {int: '-9007199254740991', float: INVALID}, // safe int, too many sig. digits for float
+    '9007199254740991': {int: '9007199254740991', float: '9007199254740991'},
+    '-9007199254740991': {int: '-9007199254740991', float: '-9007199254740991'},
 
     // Well-formed floats, outside safe range
-    '-1.234567890123456e-300': {int: INVALID, float: INVALID}, // >15 significant digits
-    '+1.234567890123456e-300': {int: INVALID, float: INVALID}, // >15 significant digits
-    '1.234567890123456e-300': {int: INVALID, float: INVALID}, // >15 significant digits
+    '-1.2345678901234567e-300': {int: INVALID, float: INVALID}, // loss of precision
+    '+1.2345678901234567e-300': {int: INVALID, float: INVALID}, // loss of precision
+    '1.2345678901234567e-300': {int: INVALID, float: INVALID}, // loss of precision
     '1e-1000': {int: INVALID, float: INVALID}, // too small
-// TODO: FIX FAILING TESTS...
-    '1.8e-308': {int: INVALID, float: INVALID}, // too small
-    // '0.18e-309': {int: INVALID, float: INVALID}, // too small
-    // '18e-307': {int: INVALID, float: INVALID}, // too small
-    // '1.70000000000001e-308': {int: INVALID, float: INVALID}, // too small
+    '0.100000000000001e-309': {int: INVALID, float: INVALID}, // too small
+    '1.00000000000001e-310': {int: INVALID, float: INVALID}, // too small
+    '0.0100000000000001e-308': {int: INVALID, float: INVALID}, // too small
     '1e1000': {int: INVALID, float: INVALID}, // too large
     '1.8e308': {int: INVALID, float: INVALID}, // too large
     '0.18e309': {int: INVALID, float: INVALID}, // too large
     '18e307': {int: INVALID, float: INVALID}, // too large
-    '1.70000000000001e308': {int: INVALID, float: INVALID}, // too large
-    '0000011111222223333344444.00000': {int: INVALID, float: INVALID}, // >15 significant digits
-    '00000111112222233333.44444': {int: INVALID, float: INVALID}, // >15 significant digits
-    '10000000000000000000.00001': {int: INVALID, float: INVALID}, // >15 significant digits
-    '10000000000000010000.00000': {int: INVALID, float: INVALID}, // >15 significant digits
-    '0000010000000000000010000.00000': {int: INVALID, float: INVALID}, // >15 significant digits
-    '0000000000100000000000000.10000': {int: INVALID, float: INVALID}, // >15 significant digits
+    '1.79999999999999e308': {int: INVALID, float: INVALID}, // too large
+    '0000011111222223333344444.00000': {int: INVALID, float: INVALID}, // loss of precision
+    '00000111112222233333.44444': {int: INVALID, float: INVALID}, // loss of precision
+    '10000000000000000000.00001': {int: INVALID, float: INVALID}, // loss of precision
+    '10000000000000001000.00000': {int: INVALID, float: INVALID}, // loss of precision
+    '0000100000000000000010000.00000': {int: INVALID, float: INVALID}, // loss of precision
+    '0000000001000000000000000.01000': {int: INVALID, float: INVALID}, // loss of precision
 
     // Well-formed floats, inside safe range
     '.1': {int: INVALID, float: '0.1'},
@@ -103,11 +101,15 @@ const tests = {
     '-1.234567890e-300': {int: INVALID, float: '-1.23456789e-300'},
     '+1.234567890e-300': {int: INVALID, float: '1.23456789e-300'},
     '1.234567890e-300': {int: INVALID, float: '1.23456789e-300'},
+    '-1.234567890123456e-300': {int: INVALID, float: '-1.234567890123456e-300'},
+    '+1.234567890123456e-300': {int: INVALID, float: '1.234567890123456e-300'},
+    '1.234567890123456e-300': {int: INVALID, float: '1.234567890123456e-300'},
     '1.7e-308': {int: INVALID, float: '1.7e-308'},
     '0.17e-309': {int: INVALID, float: '1.7e-310'},
     '17e-307': {int: INVALID, float: '1.7e-306'},
     '1.69999999999999e-308': {int: INVALID, float: '1.69999999999999e-308'},
     '1.69000000000001e-308': {int: INVALID, float: '1.69000000000001e-308'},
+    '1.70000000000001e308': {int: INVALID, float: '1.70000000000001e+308'},
     '1.7e308': {int: INVALID, float: '1.7e+308'},
     '0.17e309': {int: INVALID, float: '1.7e+308'},
     '17e307': {int: INVALID, float: '1.7e+308'},
@@ -124,6 +126,10 @@ const tests = {
     '10000000000000100000.00000': {int: INVALID, float: '10000000000000100000'},
     '0000010000000000000100000.00000': {int: INVALID, float: '10000000000000100000'},
     '0000000000100000000000001.00000': {int: INVALID, float: '100000000000001'},
+    '10000000000000010000.00000': {int: INVALID, float: '10000000000000010000'},
+    '0000010000000000000010000.00000': {int: INVALID, float: '10000000000000010000'},
+    '0000000000100000000000000.10000': {int: INVALID, float: '100000000000000.1'},
+    '0000000001000000000000000.10000': {int: INVALID, float: '1000000000000000.1'},
 };
 
 
