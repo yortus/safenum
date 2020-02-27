5a29d879cba2fc70ea408a219b4e689b63b6564c
Troy Gerwien <yortus@gmail.com>
2020-02-27 17:07:09 +0800

    add README

--- src/parse-safe-float.ts
+++ src/parse-safe-float.ts
@@ -4,5 +4,5 @@ import {tryParseSafeFloat} from './try-parse-safe-float';
 export function parseSafeFloat(rawNum: string): number {
     let result = tryParseSafeFloat(rawNum);
     if (result !== undefined) return result;
-    throw new Error(`The string ${JSON.stringify(rawNum)} cannot be parsed to a number without loss of precision.`);
+    throw new RangeError(`The string ${JSON.stringify(rawNum)} cannot be parsed to a number without loss of precision.`);
 }
--- src/parse-safe-int.ts
+++ src/parse-safe-int.ts
@@ -4,5 +4,5 @@ import {tryParseSafeInt} from './try-parse-safe-int';
 export function parseSafeInt(rawNum: string): number {
     let result = tryParseSafeInt(rawNum);
     if (result !== undefined) return result;
-    throw new Error(`The string ${JSON.stringify(rawNum)} cannot be parsed to a safe integer.`);
+    throw new RangeError(`The string ${JSON.stringify(rawNum)} cannot be parsed to a safe integer.`);
 }
